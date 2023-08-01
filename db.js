f513cbb9f75bdb5b13c7435b2174e4e830ac23bf
Roman Eduardo Gasparini <regasparini@sofredigital.com.ar>
2023-08-01 08:48:20 -0300

    update files

--- db.js
+++ db.js
@@ -1,5 +1,11 @@
 const projects = require("./data/projects");
+const openProjects = require("./data/openProjects");
+const closeProjects = require("./data/closeProjects");
+const projectsPage = require("./data/projectsPage");
 
 module.exports = {
-    projects
-}
+  projects,
+  openProjects,
+  closeProjects,
+  projectsPage
+};
