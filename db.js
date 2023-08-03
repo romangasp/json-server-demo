4975970ead99ec31b1c79c8a9cbaefde7052b657
Roman Eduardo Gasparini <regasparini@sofredigital.com.ar>
2023-08-03 18:20:23 -0300

    add purchase order

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ const projects = require("./data/projects");
 const openProjects = require("./data/openProjects");
 const closeProjects = require("./data/closeProjects");
 const projectsPage = require("./data/projectsPage");
+const purchaseOrder = require("./data/purchaseOrder")
 
 module.exports = {
   projects,
   openProjects,
   closeProjects,
-  projectsPage
+  projectsPage,
+  purchaseOrder
 };
